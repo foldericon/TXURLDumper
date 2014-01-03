ffb8d2fc551759e95ae38b4c3b9d45a15de61167
foldericon <foldericon@gmail.com>
2014-01-03 08:11:12 +0100

    > 4.0.3 compatibility.
    This breaks older versions, will add a fallback later.

--- Classes/TXURLDumper.m
+++ Classes/TXURLDumper.m
@@ -47,10 +47,10 @@ TXDumperSheet *dumperSheet;
 
     NSMenu *windowMenu = [[[[NSApplication sharedApplication] mainMenu] itemWithTitle:@"Window"] submenu];
 
-    NSMenuItem *menuItem = [NSMenuItem menuItemWithTitle:@"URL List" target:self action:@selector(showDumper:) keyEquivalent:@"6" keyEquivalentMask:NSControlKeyMask];
+    NSMenuItem *menuItem = [NSMenuItem menuItemWithTitle:@"URL List" target:self action:@selector(showDumper:) keyEquivalent:@"7" keyEquivalentMask:NSControlKeyMask];
     int i=0;
     for (NSMenuItem *item in [windowMenu itemArray]) {
-        if([item.title isEqualTo:@"Highlight List"]){
+        if([item.title isEqualTo:@"File Transfers"]){
             break;
         }
         i++;
