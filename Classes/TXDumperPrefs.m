8b326df21abc6bb4b471421317505969bcf55189
foldericon <foldericon@gmail.com>
2014-01-27 11:51:04 +0100

    Update header comments.

--- Classes/TXDumperPrefs.m
+++ Classes/TXDumperPrefs.m
@@ -1,6 +1,6 @@
 /*
  ===============================================================================
- Copyright (c) 2013, Tobias Pollmann (foldericon)
+ Copyright (c) 2013-2014, Tobias Pollmann (foldericon)
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
