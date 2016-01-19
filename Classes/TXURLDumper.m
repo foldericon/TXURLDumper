ef0d0b4805122d9eabd53ad1160e0266942505aa
foldericon <foldericon@gmail.com>
2016-01-19 21:08:19 +0100

    Use constants to check for line type

--- Classes/TXURLDumper.m
+++ Classes/TXURLDumper.m
@@ -121,7 +121,7 @@
     NSInteger lineType = (long)[[messageInfo objectForKey:@"lineType"] integerValue];
     
     // We want regular messages and actions only.
-    if(lineType != 1 && lineType != 17) return;
+    if(lineType != TVCLogLineActionType && lineType != TVCLogLinePrivateMessageType) return;
     
     IRCClient *client = logController.associatedClient;
     NSString *channel = logController.associatedChannel.name;
